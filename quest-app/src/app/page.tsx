09e8ff7a2b65e8c1b2994f84020b8e5819a0f217
Stefan Tolken <stefan@tolken.io>
2025-03-30 14:02:58 +0200

    Login and signup functionality
    - I used AWS Cognito to create the login/logout and sign up functionality
    - I created an AuthGuard to prevent unauthorized user access
    - I created functionality in the callback directory to seperate admin and client routs

--- quest-app/src/app/page.tsx
+++ quest-app/src/app/page.tsx
@@ -1,25 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import AuthButton from "@/components/ui/authButton";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl font-bold m-auto">
-          Quest
-        </h1>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            href="/login"
-          >
-            <Button>Login</Button>
-          </a>
-          <a
-            href="/signup"
-          >
-            <Button>Sign Up</Button>
-          </a>
-        </div>
+        <h1>Home page</h1>
+        <AuthButton />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
