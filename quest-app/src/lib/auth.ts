b57f07e8650725371f3971ea524cb72cef3903d9
Stefan Tolken <stefan@tolken.io>
2025-04-15 13:15:45 +0200

    Updated Auth with new AWS Account details

--- quest-app/src/lib/auth.ts
+++ quest-app/src/lib/auth.ts
@@ -1,8 +1,8 @@
 import { AuthProviderProps } from "react-oidc-context";
 
 export const CognitoAuthConfig: AuthProviderProps = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_oDkGpPpBl",
-  client_id: "3buk3745f26l9rkdel07b2k6p5",
+  authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_MQ3gFksBU",
+  client_id: "4bnj35a85lf87tg75aslkk6fre",
   redirect_uri: "http://localhost:3000/callback",
   response_type: "code",
   scope: "openid profile email",
--- quest-app/src/lib/useAuth.ts
+++ quest-app/src/lib/useAuth.ts
@@ -4,11 +4,9 @@ export function useAuthState() {
     const auth = useAuth();
 
     const signOutRedirect = () => {
-        auth.removeUser();
-
-        const clientId = "3buk3745f26l9rkdel07b2k6p5";
-        const logoutUri = "http://localhost:3000";
-        const cognitoDomain = "https://us-east-1odkgpppbl.auth.us-east-1.amazoncognito.com";
+        const clientId = "4bnj35a85lf87tg75aslkk6fre";
+        const logoutUri = "<logout uri>";
+        const cognitoDomain = "https://<user pool domain>";
         window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
     };
 
