8661fba4e15e91e3221a79ab205584b1c94e95af
Stefan-Tolken <stefan@tolken.io>
2025-06-22 15:53:14 +0200

    Fixed user auth

--- quest-app/src/components/ui/authButton.tsx
+++ quest-app/src/components/ui/authButton.tsx
@@ -4,7 +4,7 @@ import { useAuthState } from "@/lib/useAuth";
 import { Button } from "./button";
 
 export default function AuthButton() {
-  const { user, isAuthenticated, isLoading, signin, signup, signout } = useAuthState();
+  const { user, isAuthenticated, isLoading, signin, signout } = useAuthState();
   const isAdmin = Array.isArray(user?.profile?.["cognito:groups"]) ? user.profile["cognito:groups"].includes("Admin") : false;
 
   if (isLoading) return <p>Loading...</p>;
@@ -21,7 +21,6 @@ export default function AuthButton() {
   ) : (
     <div className="flex flex-col w-full max-w-2xl mx-auto gap-4">
       <Button variant={"glassDark"} onClick={() => signin()}>Sign in</Button>
-      <Button variant={"glass"} onClick={() => signup()}>Sign up</Button>
     </div>
   );
 }
--- quest-app/src/lib/auth.ts
+++ quest-app/src/lib/auth.ts
@@ -1,11 +1,9 @@
 import { AuthProviderProps } from "react-oidc-context";
 
 export const CognitoAuthConfig: AuthProviderProps = {
-  authority: `https://cognito-idp.us-east-1.amazonaws.com/us-east-1_y90MOrfAw`, // Use the IDP endpoint
+  authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY!,
   client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-  redirect_uri: process.env.NEXT_PUBLIC_BASE_URL + "callback",
+  redirect_uri: process.env.NEXT_PUBLIC_BASE_URL + "/callback",
   response_type: "code",
   scope: "openid profile email",
-  loadUserInfo: false, // Cognito includes user info in ID token
-  automaticSilentRenew: false, // Disable automatic token renewal
 };
